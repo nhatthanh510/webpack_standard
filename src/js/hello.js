268a09dcb23915c989d6a8465a0762ce5afd2679
Thanh Nguyen <nhatthanh510@gmail.com>
2016-12-06 23:17:56 +0700

    - Load image

--- src/js/hello.js
+++ src/js/hello.js
@@ -4,7 +4,6 @@
 import React, {Component} from 'react';
 import  "../css/css1.css";
 var axios = require('axios');
-import image1 from "../images/2.jpg"
 
 class App extends Component {
     constructor(props) {
@@ -13,37 +12,22 @@ class App extends Component {
             loading:"Please click the 'Random Images' button"}
         this.loadMovies = this.loadMovies.bind(this);
     }
+
     loadMovies() {
     var apiKey = '7b51cd830fc4c738968af32a4d6e13b2';
     var apiRoot = 'https://api.themoviedb.org/3';
     var apiCategory = '/discover/movie';
-    var apiUrl = `${apiRoot}${apiCategory}?${apiKey}`;
+    var apiUrl = `${apiRoot}${apiCategory}?api_key=${apiKey}`;
     var session_id = "";
     var request_token ="";
     var username = "nhatthanh510";
     var password = "shadowfiendhai2";
 
-    axios.get(`https://api.themoviedb.org/3/authentication/token/new?api_key=${apiKey}`)
-        .then((response) => {
-            console.log(response.data);
-            request_token = response.data.request_token;
-            axios.get(`https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=${apiKey}&username=${username}&password=${password}&request_token=${request_token}`)
-                .then((response) => {
-                    console.log(response.data);
-                    request_token = response.data.request_token;
-                });
-            axios.get(`https://api.themoviedb.org/3/authentication/session/new?api_key=${apiKey}&request_token=${request_token}`)
-                .then((response) => {
-                    console.log(response.data);
-                    session_id = response.data.session_id;
-                });
-        });
-
     axios.get(apiUrl)
         .then((response) => {
-            console.log(response.data);
+            console.log(response.data.results);
             console.log(response.status);
-            this.setState({data: response.results, loading: "loaded"});
+            this.setState({data: response.data.results, loading: "loaded"});
         });
     }
 
@@ -60,11 +44,13 @@ class App extends Component {
 }
 
 function MovieList(props) {
+    var imgRootPath = 'https://image.tmdb.org/t/p/';
+    var size = 'w342';
     return(
         <div>
             {props.images.map((image)=>
                 <img key ={image.title}
-                     src={image.poster_path}
+                     src={`${imgRootPath}${size}${image.poster_path}`}
                      style={{height:"200px"}}/>)}
         </div>
     )
